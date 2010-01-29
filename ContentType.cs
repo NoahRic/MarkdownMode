59000d42f4c3770075d8ae8fb61980d22c2b6e2d
Noah Richards <noahric@microsoft.com>
2010-01-29 15:23:19 -0800

    Added explicit HTML content type declaration, to work around a Beta 2 bug.  Bumped the version number to 0.2.

--- ContentType.cs
+++ ContentType.cs
@@ -14,6 +14,13 @@ namespace MarkdownMode
         [BaseDefinition("HTML")]
         public static ContentTypeDefinition MarkdownModeContentType = null;
 
+        // Beta 2 workaround:
+        // We need to also explicitly declare HTML, or else our declaration will cause HTML to break
+        [Export]
+        [Name("HTML")]
+        [BaseDefinition("code")]
+        public static ContentTypeDefinition HTMLContentType = null;
+
         [Export]
         [ContentType(Name)]
         [FileExtension(".mkd")]
