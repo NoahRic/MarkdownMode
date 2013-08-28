7ca37003695aa523fa49b39ac569208d42e322d7
Sam Harwell <sam@tunnelvisionlabs.com>
2013-08-27 22:32:03 -0500

    Make the GitHub authorization form larger to reduce the chance of requiring scrolling

--- OptionsPages/GitHubAuthorizationForm.Designer.cs
+++ OptionsPages/GitHubAuthorizationForm.Designer.cs
@@ -37,7 +37,7 @@
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(676, 403);
+            this.webBrowser1.Size = new System.Drawing.Size(1039, 602);
             this.webBrowser1.TabIndex = 0;
             this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
             this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 403);
+            this.ClientSize = new System.Drawing.Size(1039, 602);
             this.Controls.Add(this.webBrowser1);
             this.Name = "GitHubAuthorizationForm";
             this.Text = "Github Authorization";
